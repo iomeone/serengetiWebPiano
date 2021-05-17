530174754584297588e046655e6977d94e208b36
chlorUpload <chlor.upload@gmail.com>
2021-05-18 00:43:50 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Space, Typography } from "antd";
+import { Footer, Header } from "antd/lib/layout/layout";
+import styled from "styled-components";
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+const Screen = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const FooterCont = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Screen>
+      <Header>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Space size={8}>
+            <Typography.Text
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              Serengeti
+            </Typography.Text>
+            <Typography.Text
+              style={{
+                color: "white",
+                fontSize: 18,
+              }}
+            >
+              | UGRP
+            </Typography.Text>
+          </Space>
+        </div>
+      </Header>
+      <Main></Main>
+      <Footer>
+        <FooterCont>
+          <Typography.Text style={{ textAlign: "center" }}>
+            copyright. 2021. sabana-music
+          </Typography.Text>
+        </FooterCont>
+      </Footer>
+    </Screen>
   );
 }
 
