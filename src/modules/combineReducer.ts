387068b247cdf7f26e161c23baf56726ddf24061
chlorUpload <chlor.upload@gmail.com>
2021-06-18 22:56:07 +0900

    sheetReducer 추가

--- src/modules/combineReducer.ts
+++ src/modules/combineReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { counterReducer } from "modules/counter";
+import { sheetReducer } from "./sheet";
 
 export default combineReducers({
   counter: counterReducer,
+  sheet: sheetReducer,
 });
