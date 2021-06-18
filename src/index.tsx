9b9c54a1ce7c716ce2d1ef3072e50d5c2244a85b
chlorUpload <chlor.upload@gmail.com>
2021-06-18 23:24:21 +0900

    redux-thunk 통합

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { Provider as ReduxProvider } from "react-redux";
-import combineReducer from "modules/combineReducer";
-import initialState from "modules/initialState";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider as ReduxProvider } from 'react-redux';
+import combineReducer from 'modules/combineReducer';
+import initialState from 'modules/initialState';
+import ReduxThunk from 'redux-thunk';
 
 let store;
-if (process.env.NODE_ENV === "production") {
-  store = createStore(combineReducer, initialState);
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(
+    combineReducer,
+    initialState,
+    applyMiddleware(ReduxThunk),
+  );
 } else {
-  store = createStore(combineReducer, initialState, composeWithDevTools());
+  store = createStore(
+    combineReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(ReduxThunk)),
+  );
 }
 
 ReactDOM.render(
@@ -22,7 +31,7 @@ ReactDOM.render(
       <App />
     </ReduxProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
