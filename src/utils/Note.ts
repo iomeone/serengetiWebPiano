754964af62ed91e4e3aa1e74c523496abdb83e39
chlorUpload <chlor.upload@gmail.com>
2021-08-03 00:34:55 +0900

    Note 유틸 추가

---
+++ src/utils/Note.ts
@@ -0,0 +1,49 @@
+export enum PitchClass {
+  C,
+  Cis,
+  D,
+  Dis,
+  E,
+  F,
+  Fis,
+  G,
+  Gis,
+  A,
+  Ais,
+  B,
+}
+
+const pitchClassArr = Object.entries(PitchClass).map((p) => p[1]) as string[];
+
+export type Note = {
+  pitchClass: PitchClass;
+  octave: number;
+};
+
+export const midiKeyNumberToNote = (midiKeyNumber: number): Note => {
+  let octave = Math.floor(midiKeyNumber / 12) - 1;
+  let pitchClass = midiKeyNumber % 12;
+  return { pitchClass, octave };
+};
+
+export const noteToMidiKeyNumber = (note: Note): number => {
+  return (note.octave + 1) * 12 + note.pitchClass;
+};
+
+export const noteToNoteName = (note: Note): string => {
+  return pitchClassArr[note.pitchClass] + note.octave;
+};
+
+export const parseNoteNameToNote = (noteName: string): Note => {
+  const noteReg = /^([A-Za-z]+)([0-9]+)$/;
+  const ret = noteReg.exec(noteName);
+  if (ret === null || ret.length < 3) throw 'parse failed';
+
+  const className = ret[1];
+  const pitchClass = pitchClassArr.findIndex((str) => str === className);
+  if (pitchClass === -1) throw 'parse failed';
+  const octave = parseInt(ret[2]);
+  if (isNaN(octave)) throw 'parse failed';
+
+  return { pitchClass, octave };
+};
