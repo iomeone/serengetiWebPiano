8970644b671bca03c7b655430d6f7ba45b106762
chlorUpload <chlor.upload@gmail.com>
2021-08-07 19:32:24 +0900

    온음계 숫자 추가

--- src/utils/Note.ts
+++ src/utils/Note.ts
@@ -1,5 +1,5 @@
 export enum PitchClass {
-  C,
+  C = 0,
   Cis,
   D,
   Dis,
@@ -51,4 +51,74 @@ export const parseNoteNameToNote = (noteName: string): Note => {
   if (isNaN(octave)) throw 'parse failed';
 
   return { pitchClass, octave };
+};
+
+export function isNaturalNote(note: Note): boolean {
+  switch (note.pitchClass) {
+    case PitchClass.Ais:
+    case PitchClass.Cis:
+    case PitchClass.Dis:
+    case PitchClass.Fis:
+    case PitchClass.Gis:
+      return false;
+    default:
+      return true;
+  }
+}
+
+export function noteToDiatonicNumber(note: Note): number {
+  const pitchClassArr: { pitchClass: PitchClass; ind: number }[] = [
+    {
+      pitchClass: PitchClass.Cis,
+      ind: 0,
+    },
+    { pitchClass: PitchClass.Dis, ind: 1 },
+    { pitchClass: PitchClass.E, ind: 2 },
+    { pitchClass: PitchClass.Fis, ind: 3 },
+    { pitchClass: PitchClass.Gis, ind: 4 },
+    { pitchClass: PitchClass.Ais, ind: 5 },
+    { pitchClass: PitchClass.B, ind: 6 },
+  ];
+  for (const entry of pitchClassArr) {
+    if (note.pitchClass <= entry.pitchClass) return entry.ind + note.octave * 7;
+  }
+  return -1;
+}
+
+export function diatonicNumberToNote(diatonicNumber: number): Note {
+  const map = {
+    0: PitchClass.C,
+    1: PitchClass.D,
+    2: PitchClass.E,
+    3: PitchClass.F,
+    4: PitchClass.G,
+    5: PitchClass.A,
+    6: PitchClass.B,
+  };
+  const diatonicClass = diatonicNumber % 7;
+  const octave = Math.floor(diatonicNumber / 7);
+
+  return {
+    octave,
+    pitchClass: map[diatonicClass as keyof typeof map],
+  };
+}
+
+export const parseNoteNameToDiatonicNumber = (noteName: string): number => {
+  const noteReg = /^([A-Za-z]+)([0-9]+)$/;
+  const ret = noteReg.exec(noteName);
+  if (ret === null || ret.length < 3) throw 'parse failed';
+
+  const className = ret[1];
+  const pitchClass = pitchClassArr.findIndex((str) => str === className);
+  if (pitchClass === -1) throw 'parse failed';
+  const octave = parseInt(ret[2]);
+  if (isNaN(octave)) throw 'parse failed';
+
+  return noteToDiatonicNumber({ pitchClass, octave });
+};
+
+export const diatonicNumberToNoteName = (diatonicNumber: number): string => {
+  const note = diatonicNumberToNote(diatonicNumber);
+  return pitchClassArr[note.pitchClass] + note.octave;
 };
