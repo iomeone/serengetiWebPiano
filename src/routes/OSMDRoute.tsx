1892fc8908988c296676337f58150ff0e4db8d66
chlorUpload <chlor.upload@gmail.com>
2021-08-08 01:26:26 +0900

    usePiano의 테스트 코드 작성

--- src/routes/OSMDRoute.tsx
+++ src/routes/OSMDRoute.tsx
@@ -1,5 +1,12 @@
-import { Typography } from 'antd';
+import { Button, Space, Typography } from 'antd';
+import { useEffect, useState } from 'react';
+import { FrontAudioService } from 'services/FrontAudioService';
+import { Articulation } from 'services/IAudioService';
 import styled from 'styled-components';
+
+import * as Soundfont from 'soundfont-player';
+import { AudioContext } from 'standardized-audio-context';
+import { usePiano } from 'hooks/usePiano';
 
 const margin = 20;
 
@@ -8,9 +15,25 @@ const Main = styled.div`
 `;
 
 export default function OSMDRoute() {
+  const play = usePiano();
+
   return (
     <Main>
-      <Typography.Text>OSMD Component Test Page</Typography.Text>
+      <Space direction="vertical" size={8} style={{ width: '100%' }}>
+        <Typography.Text>OSMD Component Test Page</Typography.Text>
+        <Button
+          onClick={() => {
+            play({
+              articulation: Articulation.Legato,
+              duration: 1,
+              gain: 3,
+              midiKeyNumber: 50,
+            });
+          }}
+        >
+          play sound
+        </Button>
+      </Space>
     </Main>
   );
 }
