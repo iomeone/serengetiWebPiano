1a101d331a7997e0305e77b8ed1e19ca838172c0
chlorUpload <chlor.upload@gmail.com>
2021-08-20 01:53:15 +0900

    구현되지 않은 Action의 reducer 행동 구현

--- src/modules/audio.ts
+++ src/modules/audio.ts
@@ -4,38 +4,29 @@ import { AudioState, State } from 'modules/State';
 import inistialState from './initialState';
 import { OpenSheetMusicDisplay as OSMD } from 'opensheetmusicdisplay';
 import { IAudioContext } from 'standardized-audio-context';
+import { Sheet } from 'models/Sheet';
 
 export const ADD_SHEET = '@AUDIO/ADD_SHEET';
 export const addSheet = (key: string, osmd: OSMD) =>
   action(ADD_SHEET, { key, osmd });
-type AddSheet = ActionType<typeof addSheet>;
-
-export const SET_FILE = '@AUDIO/SET_FILE';
-export const _setFile = (key: string, file: File) =>
-  action(SET_FILE, { key, file });
-type SetFile = ActionType<typeof _setFile>;
+export type AddSheet = ActionType<typeof addSheet>;
 
 export const SET_TITLE = '@AUDIO/SET_TITLE';
 export const _setTitle = (key: string, title: string) =>
   action(SET_TITLE, { key, title });
-type SetTitle = ActionType<typeof _setTitle>;
+export type SetTitle = ActionType<typeof _setTitle>;
 
 export const SET_LOADED = '@AUDIO/SET_LOADED';
 export const _setLoaded = (key: string, loaded: boolean) =>
   action(SET_LOADED, { key, loaded });
-type SetLoaded = ActionType<typeof _setLoaded>;
+export type SetLoaded = ActionType<typeof _setLoaded>;
 
 export const SET_AUDIO_CONTEXT = '@AUDIO/SET_AUDIO_CONTEXT';
 export const setAudioContext = (audioContext: IAudioContext) =>
   action(SET_AUDIO_CONTEXT, { audioContext });
-type SetAudioContext = ActionType<typeof setAudioContext>;
+export type SetAudioContext = ActionType<typeof setAudioContext>;
 
-export type AudioActions =
-  | AddSheet
-  | SetFile
-  | SetTitle
-  | SetLoaded
-  | SetAudioContext;
+export type AudioActions = AddSheet | SetTitle | SetLoaded | SetAudioContext;
 
 /* thunks */
 
@@ -49,7 +40,6 @@ export const loadSheetThunk =
       return;
     }
     dispatch(_setTitle(key, file.name));
-    dispatch(_setFile(key, file));
     dispatch(_setLoaded(key, true));
     osmd.render();
   };
@@ -77,19 +67,32 @@ export const audioReducer = (
     case ADD_SHEET: {
       const { payload } = action as AddSheet;
       return produce<AudioState>(state, (draft) => {
-        const sheet = {
+        const sheet: Sheet = {
           osmd: payload.osmd as any,
-          file: null,
           title: null,
           loaded: false,
         };
         draft.sheets[payload.key] = sheet;
       });
     }
-    case SET_FILE: {
-      const { payload } = action as SetFile;
+    case SET_TITLE: {
+      const { payload } = action as SetTitle;
       return produce<AudioState>(state, (draft) => {
-        draft.sheets[payload.key].file = payload.file;
+        const sheet = draft.sheets[payload.key] as Sheet;
+        sheet.title = payload.title;
+      });
+    }
+    case SET_LOADED: {
+      const { payload } = action as SetLoaded;
+      return produce<AudioState>(state, (draft) => {
+        const sheet = draft.sheets[payload.key] as Sheet;
+        sheet.loaded = payload.loaded;
+      });
+    }
+    case SET_AUDIO_CONTEXT: {
+      const { payload } = action as SetAudioContext;
+      return produce<AudioState>(state, (draft) => {
+        draft.audioContext = payload.audioContext;
       });
     }
     default:
