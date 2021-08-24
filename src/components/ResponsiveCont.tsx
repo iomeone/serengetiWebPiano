36b0201ab515a6c1db22f136331e2ce4141e3081
chlorUpload <chlor.upload@gmail.com>
2021-08-24 22:31:44 +0900

    반응형을 단계별로 통일감 있게 구현하기 위한 컴포넌트 추가

---
+++ src/components/ResponsiveCont.tsx
@@ -0,0 +1,43 @@
+import { Size, WidthMode } from 'constants/layout';
+import { useWidth } from 'hooks/useWidth';
+import styled, { css } from 'styled-components';
+
+const Cont = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  background: ${(props) => props.color};
+  justify-self: end;
+`;
+
+const Inner = styled.div`
+  ${(props: { mode: WidthMode }) => {
+    switch (props.mode) {
+      case WidthMode.Desktop:
+        return css`
+          width: ${Size.maxWidth}px;
+          height: 100%;
+        `;
+      case WidthMode.Tablet:
+      case WidthMode.Mobile:
+        return css`
+          width: ${`calc(100% - ${2 * Size.hMargin}px)`};
+          height: 100%;
+        `;
+    }
+  }}
+`;
+
+export default function ResponsiveCont({ color, children }: Props) {
+  const { widthMode } = useWidth();
+  return (
+    <Cont color={color ?? 'white'}>
+      <Inner mode={widthMode}>{children}</Inner>
+    </Cont>
+  );
+}
+
+type Props = {
+  color?: string;
+  children: React.ReactNode;
+};
