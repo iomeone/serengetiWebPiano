605cfa0b42ce17b1031aaad08830db42f97ff1a6
chlorUpload <chlor.upload@gmail.com>
2021-08-25 22:22:42 +0900

    PlaybackService를 OSMD별로 저장하도록 변경, redux debugging을 원할히 수행하기 위한 새니타이저 작성

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,11 @@ import ReduxThunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import { State } from 'modules/State';
 import produce from 'immer';
+import {
+  ADD_SHEET,
+  SET_AUDIO_CONTEXT,
+  SET_PLAYBACK_SERVICE,
+} from 'modules/audio';
 
 let store;
 if (process.env.NODE_ENV === 'production') {
@@ -24,10 +29,21 @@ if (process.env.NODE_ENV === 'production') {
   const options = {
     actionSanitizer: (action: any) => {
       switch (action.type) {
-        case '@AUDIO/ADD_SHEET':
+        case ADD_SHEET:
           return { ...action, osmd: 'OSMD Object' };
-        case '@AUDIO/SET_AUDIO_CONTEXT':
-          return { ...action, audioContext: 'Audio Context Object' };
+        case SET_AUDIO_CONTEXT:
+          return {
+            ...action,
+            audioContext: 'Audio Context Object',
+          };
+        case SET_PLAYBACK_SERVICE:
+          return {
+            ...action,
+            playbackService:
+              action.playbackService !== null
+                ? 'Playback Service Object'
+                : null,
+          };
         default:
           return action;
       }
@@ -35,11 +51,17 @@ if (process.env.NODE_ENV === 'production') {
     stateSanitizer: (state: any) => {
       return produce(state as State, (draft) => {
         for (const key of Object.keys(draft.audio.sheets)) {
-          draft.audio.sheets[key].osmd = 'OSMD Object';
+          if (draft.audio.sheets[key].osmd !== null)
+            draft.audio.sheets[key].osmd = 'OSMD Object';
+          if (draft.audio.sheets[key].playbackService !== null) {
+            //@ts-ignore
+            draft.audio.sheets[key].playbackService = 'Playback Service Object';
+          }
         }
-        if (draft.audio.audioContext !== null)
+        if (draft.audio.audioContext !== null) {
           //@ts-ignore
           draft.audio.audioContext = 'Audio Context Object';
+        }
       }) as any;
     },
   };
