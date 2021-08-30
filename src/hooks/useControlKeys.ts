4e6cedebafdc80b0b6b54eeb0a9bfe4f493d13e6
chlorUpload <chlor.upload@gmail.com>
2021-08-31 08:03:39 +0900

    컨트롤 키 등의 키 정보를 받아오는 custom hook 추가

---
+++ src/hooks/useControlKeys.ts
@@ -0,0 +1,62 @@
+import { useEffect, useMemo, useState } from 'react';
+
+type KeyMap = {
+  [key: string]: boolean | undefined;
+};
+
+type UseControlKeysRes = {
+  keys: KeyMap;
+  ctrlZ: boolean;
+  ctrlY: boolean;
+};
+
+export function useControlKeys(): UseControlKeysRes {
+  const [keys, setKeys] = useState<KeyMap>({});
+
+  const onKeyDown = (key: any) => {
+    setKeys((ks) => ({
+      ...ks,
+      [key]: true,
+    }));
+  };
+  const onKeyUp = (key: any) => {
+    setKeys((ks) => ({
+      ...ks,
+      [key]: false,
+    }));
+  };
+
+  const keyDownHandler = ({ key, repeat }: KeyboardEvent) => {
+    if (!repeat) {
+      onKeyDown(key);
+    }
+  };
+
+  const keyUpHandler = ({ key }: KeyboardEvent) => {
+    onKeyUp(key);
+  };
+
+  const ctrlZ = useMemo(
+    () => (keys['Control'] ?? false) && (keys['z'] ?? false),
+    [keys],
+  );
+  const ctrlY = useMemo(
+    () => (keys['Control'] ?? false) && (keys['y'] ?? false),
+    [keys],
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler);
+    window.addEventListener('keyup', keyUpHandler);
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler);
+      window.removeEventListener('keyup', keyUpHandler);
+    };
+  }, []);
+
+  return {
+    keys,
+    ctrlZ,
+    ctrlY,
+  };
+}
