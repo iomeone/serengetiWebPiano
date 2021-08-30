19044b5653cc66d59f6058aec0fabea81914e467
chlorUpload <chlor.upload@gmail.com>
2021-08-31 08:04:49 +0900

    Indexed 타입과 Indexed 객체를 만들기 위한 함수 추가

--- src/utils/List.tsx
+++ src/utils/List.tsx
@@ -8,3 +8,15 @@ export function swap<T>(arr: T[], index1: number, index2: number): T[] {
     [draft[index1], draft[index2]] = [draft[index2], draft[index1]];
   });
 }
+
+export type Indexed<T> = {
+  content: T;
+  key: number;
+};
+
+export function index<T>(array: T[]): Indexed<T>[] {
+  return array.map((item, ind) => ({
+    key: ind,
+    content: item,
+  }));
+}
