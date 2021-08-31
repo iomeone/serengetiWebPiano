4940bb5697cf3c4ffc8e2a352dc2eb6249447381
psm7177 <raspberry-pi@kakao.com>
2021-08-31 23:48:28 +0900

    xml 업로드 추가

--- src/routes/OSMDEditorRoute.tsx
+++ src/routes/OSMDEditorRoute.tsx
@@ -1,3 +1,36 @@
+import { UploadOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import LoadSheetModal from 'components/LoadSheetModal';
+import ResponsiveCont from 'components/ResponsiveCont';
+import { useState } from 'react';
+
 export default function OSMDEditorRoute() {
-    return <div></div>
+  const [loadModal, setLoadModal] = useState(false);
+  const [sheetFile, setSheetFile] = useState<File | null>(null);
+   
+  return (
+    <>
+      <LoadSheetModal
+        onLoadFile={(file) => {
+          setSheetFile(file.originFileObj as File);
+        }}
+        visible={loadModal}
+        onVisibleChange={setLoadModal}
+      ></LoadSheetModal>
+      <ResponsiveCont>
+        <Button 
+            style={{
+                margin:"auto 0px"
+            }}
+          onClick={() => {
+            setLoadModal(true);
+          }}
+        >
+          <UploadOutlined />
+          악보 업로드
+        </Button>
+        
+      </ResponsiveCont>
+    </>
+  );
 }
