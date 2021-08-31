4f34ba6c70bcdd367413118cb2caf1ce8ab72267
psm7177 <raspberry-pi@kakao.com>
2021-08-31 21:06:28 +0900

    원상복귀

--- src/hooks/usePiano.ts
+++ src/hooks/usePiano.ts
@@ -1,18 +1,12 @@
 import { useMemo } from 'react';
 import { FrontAudioService } from 'services/FrontAudioService';
 import { NotePlayOption } from 'services/IAudioService';
-import { binaryKeysToNoteArray, Note } from 'utils/Note';
-import { useBinaryPressedKeys } from './useBinaryPressedKeys';
 import { useFrontAudioService } from './useFrontAudioService';
-import { useFrontMIDIAudio } from './useFrontMIDIAudio';
-import { useKeyboardMIDI } from './useKeyboardMIDI';
 
 type PianoRes = {
   preloadWithGesture: () => void;
   isLoaded: boolean;
   play: (note: NotePlayOption) => Promise<void>;
-  pressedKeys: Note[];
-  pressedBinaryKeys: boolean[];
 };
 
 export function usePiano(): PianoRes {
@@ -20,16 +14,10 @@ export function usePiano(): PianoRes {
     frontAudioService,
     getOrCreateFrontAudioServiceWithGesture: preloadWithGesture,
   } = useFrontAudioService();
-  const { onKeyUp, onKeyDown, pressedBinaryKeys } = useBinaryPressedKeys();
-  const { initWithGesture, isMIDIConnected, isMIDISupported } =
-    useFrontMIDIAudio(
-      (note: number) => {
-        onKeyDown(note);
-      },
-      (note: number) => {
-        onKeyUp(note);
-      },
-    );
+
+  const isLoaded = useMemo(() => {
+    return frontAudioService !== null;
+  }, [frontAudioService]);
 
   const play = async (note: NotePlayOption) => {
     let fas: FrontAudioService | null = frontAudioService;
@@ -40,25 +28,5 @@ export function usePiano(): PianoRes {
     fas?.play(note);
   };
 
-  const { pressedBinaryKeysByKeyboard } = useKeyboardMIDI(play);
-  const integratedPressedBinaryKeys = useMemo(() => {
-    return pressedBinaryKeys.map(
-      (value, index) => value || pressedBinaryKeysByKeyboard[index],
-    );
-  }, [pressedBinaryKeys, pressedBinaryKeysByKeyboard]);
-  const integratedPressedKeys = useMemo(() => {
-    return binaryKeysToNoteArray(integratedPressedBinaryKeys);
-  }, [integratedPressedBinaryKeys]);
-
-  const isLoaded = useMemo(() => {
-    return frontAudioService !== null;
-  }, [frontAudioService]);
-
-  return {
-    preloadWithGesture,
-    isLoaded,
-    play,
-    pressedKeys: integratedPressedKeys,
-    pressedBinaryKeys: integratedPressedBinaryKeys,
-  };
+  return { preloadWithGesture, isLoaded, play };
 }
