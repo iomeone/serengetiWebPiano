29d2694154705d4505cca09273c75bdec78f5553
chlorUpload <chlor.upload@gmail.com>
2021-08-31 11:01:27 +0900

    이미지 업로드 영역 컴포넌트 추가

---
+++ src/components/ImageUploadArea.tsx
@@ -0,0 +1,65 @@
+import { InboxOutlined } from '@ant-design/icons';
+import { Button, Space, Typography, Upload } from 'antd';
+import { UploadFile } from 'antd/lib/upload/interface';
+import { useCallback, useEffect, useState } from 'react';
+
+type Props = {
+  file?: UploadFile<any> | null;
+  onFileChange?: (file: UploadFile<any> | null) => void;
+  onLoadFile: (file: UploadFile<any>) => void;
+};
+
+export default function ImageUploadArea({
+  file,
+  onFileChange,
+  onLoadFile,
+}: Props) {
+  const [myFile, setMyFile] = useState<UploadFile<any> | null>(null);
+
+  useEffect(() => {
+    if (file !== undefined) setMyFile(file);
+  }, [file, onFileChange]);
+
+  const onMyFileChange = useCallback(
+    (newFile: UploadFile<any>) => {
+      if (onFileChange !== undefined) {
+        onFileChange(newFile);
+      }
+      setMyFile(newFile);
+    },
+    [setMyFile, onFileChange],
+  );
+
+  return (
+    <Space direction="vertical" size={8} style={{ width: '100%' }}>
+      <Typography.Text>이미지 파일을 업로드해주세요.</Typography.Text>
+      <Upload.Dragger
+        name="file"
+        multiple={false}
+        customRequest={(e) => {
+          if (e.onSuccess) e.onSuccess({}, new XMLHttpRequest());
+        }}
+        onChange={(e) => {
+          onMyFileChange(e.file ?? null);
+        }}
+        fileList={myFile === null ? [] : [myFile]}
+      >
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">클릭하거나 파일을 드래그해서 업로드</p>
+        <p className="ant-upload-hint">1개의 이미지 파일을 업로드해주세요.</p>
+      </Upload.Dragger>
+      <Typography.Text>업로드 완료 후 확인 버튼을 눌러주세요.</Typography.Text>
+      <Button
+        type="primary"
+        disabled={myFile === null}
+        onClick={() => {
+          if (myFile !== null) onLoadFile(myFile);
+        }}
+      >
+        확인
+      </Button>
+    </Space>
+  );
+}
