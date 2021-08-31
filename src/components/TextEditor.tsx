3565e727c3d059e761a2014fb71583e2e7bb6ab4
chlorUpload <chlor.upload@gmail.com>
2021-08-31 11:02:40 +0900

    제목 등에 사용되는 텍스트 편집 컴포넌트 추가

---
+++ src/components/TextEditor.tsx
@@ -0,0 +1,99 @@
+import { EditOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Typography } from 'antd';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Horizontal from './Horizontal';
+
+type Props = {
+  tag: string;
+  title: string;
+  onSubmit: (nextTitle: string) => void;
+};
+
+export default function TextEditor({ tag, title, onSubmit }: Props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [myTitle, setMyTitle] = useState('');
+
+  useEffect(() => {
+    if (title !== undefined) {
+      setMyTitle(title);
+    }
+  }, [title]);
+
+  const closeTitleChanger = () => {
+    setIsEditing(false);
+  };
+  const prepareChangeTitle = () => {
+    setIsEditing(true);
+    setMyTitle(title ?? '');
+  };
+  const onMyTitleChange = (nextTitle: string) => {
+    setMyTitle(nextTitle);
+  };
+  const submit = () => {
+    onSubmit(myTitle);
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <Horizontal>
+        <Input
+          value={myTitle}
+          onChange={(e) => {
+            onMyTitleChange(e.target.value);
+          }}
+          onPressEnter={() => {
+            submit();
+          }}
+        ></Input>
+        <Space
+          direction="horizontal"
+          size={6}
+          style={{
+            marginLeft: 6,
+          }}
+        >
+          <Button
+            type="primary"
+            onClick={() => {
+              submit();
+            }}
+          >
+            변경
+          </Button>
+          <Button
+            type="primary"
+            onClick={() => {
+              closeTitleChanger();
+            }}
+          >
+            닫기
+          </Button>
+        </Space>
+      </Horizontal>
+    );
+  } else {
+    return (
+      <Space direction="horizontal" size={6}>
+        <Typography.Text
+          style={{
+            fontWeight: 'bold',
+          }}
+        >
+          {tag} |{' '}
+        </Typography.Text>
+        <Typography.Text>{myTitle}</Typography.Text>
+        <Button
+          type="text"
+          shape="circle"
+          onClick={() => {
+            prepareChangeTitle();
+          }}
+        >
+          <EditOutlined></EditOutlined>
+        </Button>
+      </Space>
+    );
+  }
+}
