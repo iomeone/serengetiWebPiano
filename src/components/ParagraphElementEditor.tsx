9d25d3f6e70cdb6618809dc1782539603aab1fd6
chlorUpload <chlor.upload@gmail.com>
2021-08-31 20:13:30 +0900

    Paragraph Editor 좀 사람이 쓸만한 상태로 수정함

--- src/components/ParagraphElementEditor.tsx
+++ src/components/ParagraphElementEditor.tsx
@@ -1,21 +1,22 @@
-import { PlusOutlined } from '@ant-design/icons';
-import { Button, Input, List, message, Space, Typography } from 'antd';
+import {
+  CheckOutlined,
+  DeleteOutlined,
+  LoadingOutlined,
+  PlusOutlined,
+} from '@ant-design/icons';
+import { Button, Space } from 'antd';
 import { useEditor } from 'hooks/useEditor';
 import produce from 'immer';
-import { Paragraph } from 'models/Worksheet';
-import { useRef } from 'react';
+import { EditorParagraph, Paragraph } from 'models/Worksheet';
+import { useEffect } from 'react';
 import { useState } from 'react';
-import { index, Indexed } from 'utils/List';
+import styled from 'styled-components';
+import ContentEditable from './ContentEditable';
 import Horizontal from './Horizontal';
 
 type ParagraphElementEditorProps = {
   elem: Paragraph;
   elemInd: number;
-};
-
-type SentenceAdder = {
-  selectedParagraphInd: number | null;
-  value: string;
 };
 
 export default function ParagraphElementEditor({
@@ -33,120 +34,27 @@ export default function ParagraphElementEditor({
     );
   };
 
-  const addSentence = (paragraphInd: number, sentence: string) => {
-    updateElem(
-      elemInd,
-      produce(elem, (draft) => {
-        draft.content[paragraphInd].push(sentence);
-      }),
-    );
-  };
-
-  const [sentenceAdder, setSentenceAdder] = useState<SentenceAdder>({
-    selectedParagraphInd: null,
-    value: '',
-  });
-  const sentenceAdderRef = useRef<Input>(null);
-  const prepareAddSentence = (paragraphInd: number) => {
-    setSentenceAdder({
-      selectedParagraphInd: paragraphInd,
-      value: '',
-    });
-    sentenceAdderRef.current?.focus();
-  };
-  const closeSentenceAdder = () => {
-    setSentenceAdder({
-      selectedParagraphInd: null,
-      value: '',
-    });
-  };
-  const onSentenceChange = (nextValue: string) => {
-    setSentenceAdder((adder) => ({
-      ...adder,
-      value: nextValue,
-    }));
-  };
-  const submit = (paragraphInd: number) => {
-    const sentence = sentenceAdder.value;
-
-    if (sentence.length === 0) {
-      message.error('문장을 입력해주세요.');
-    } else {
-      onSentenceChange('');
-      addSentence(paragraphInd, sentence);
-      sentenceAdderRef.current?.focus();
-    }
-  };
-
   return (
-    <List
-      dataSource={index(elem.content)}
-      renderItem={(paragraph: Indexed<string[]>) => (
-        <List.Item>
-          <Space
-            direction="vertical"
-            size={4}
-            style={{
-              width: '100%',
-            }}
-          >
-            {paragraph.content.map((str, ind) => (
-              <Typography.Text key={ind}>{str}</Typography.Text>
-            ))}
-            {sentenceAdder.selectedParagraphInd === paragraph.key ? (
-              <Horizontal>
-                <Input
-                  ref={sentenceAdderRef}
-                  value={sentenceAdder.value}
-                  onChange={(e) => {
-                    onSentenceChange(e.target.value);
-                  }}
-                  onPressEnter={() => {
-                    submit(paragraph.key);
-                  }}
-                ></Input>
-                <Space
-                  direction="horizontal"
-                  size={6}
-                  style={{
-                    marginLeft: 6,
-                  }}
-                >
-                  <Button
-                    type="primary"
-                    onClick={() => {
-                      submit(paragraph.key);
-                    }}
-                  >
-                    <PlusOutlined></PlusOutlined> 문장 추가
-                  </Button>
-                  <Button
-                    type="primary"
-                    onClick={() => {
-                      closeSentenceAdder();
-                    }}
-                  >
-                    닫기
-                  </Button>
-                </Space>
-              </Horizontal>
-            ) : (
-              <Button
-                onClick={() => {
-                  prepareAddSentence(paragraph.key);
-                }}
-              >
-                <PlusOutlined></PlusOutlined> 문장 추가
-              </Button>
-            )}
-          </Space>
-        </List.Item>
-      )}
+    <Space
+      direction="vertical"
+      size={2}
+      style={{
+        width: '100%',
+      }}
     >
+      {elem.content.map((paragraph, paragraphInd) => (
+        <ParagraphEditor
+          elem={elem}
+          paragraph={paragraph}
+          elemInd={elemInd}
+          paragraphInd={paragraphInd}
+        ></ParagraphEditor>
+      ))}
       <Button
         type="dashed"
         style={{
           width: '100%',
+          marginTop: 8,
         }}
         onClick={() => {
           addParagraph();
@@ -154,6 +62,104 @@ export default function ParagraphElementEditor({
       >
         <PlusOutlined></PlusOutlined> 문단 추가
       </Button>
-    </List>
+    </Space>
+  );
+}
+
+const ParagraphBox = styled.div`
+  width: 100%;
+  border-radius: 2px;
+  border: 1px solid #69c0ff;
+  padding: 3px;
+`;
+
+type ParagraphEditorProps = {
+  elem: EditorParagraph;
+  paragraph: string[];
+  elemInd: number;
+  paragraphInd: number;
+};
+
+function ParagraphEditor({
+  elem,
+  paragraph,
+  elemInd,
+  paragraphInd,
+}: ParagraphEditorProps) {
+  const autoSaveInterval = 800;
+  const [value, setValue] = useState('');
+  const [lastChanged, setLastChanged] = useState(Date.now());
+  const [currentTime, setCurrentTime] = useState(Date.now());
+  const [saved, setSaved] = useState(true);
+  const { updateElem } = useEditor();
+
+  const deleteParagraph = () => {
+    updateElem(
+      elemInd,
+      produce(elem, (draft) => {
+        draft.content.splice(paragraphInd, 1);
+      }),
+    );
+  };
+
+  const save = () => {
+    setSaved(true);
+    updateElem(
+      elemInd,
+      produce(elem, (draft) => {
+        draft.content[paragraphInd] = value.split('\n');
+      }),
+    );
+  };
+
+  useEffect(() => {
+    if (!saved) {
+      if (currentTime - lastChanged > autoSaveInterval) {
+        save();
+      }
+    }
+  }, [saved, lastChanged, currentTime]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(Date.now());
+    }, autoSaveInterval);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Horizontal>
+      <ParagraphBox>
+        <ContentEditable
+          value={paragraph.join('\n')}
+          onChange={(value) => {
+            setValue(value);
+            setSaved(false);
+            setLastChanged(Date.now());
+          }}
+        ></ContentEditable>
+      </ParagraphBox>
+      <Space
+        direction="horizontal"
+        size={6}
+        style={{
+          marginLeft: 6,
+        }}
+      >
+        {saved ? <CheckOutlined /> : <LoadingOutlined />}
+        <Button
+          type="text"
+          shape="circle"
+          onClick={() => {
+            deleteParagraph();
+          }}
+        >
+          <DeleteOutlined size={10}></DeleteOutlined>
+        </Button>
+      </Space>
+    </Horizontal>
   );
 }
