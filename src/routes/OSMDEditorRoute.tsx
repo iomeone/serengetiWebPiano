cfc9f0ee30315135cc74c3e7eab915f415095062
psm7177 <raspberry-pi@kakao.com>
2021-09-03 18:34:00 +0900

    OSMDEditor 구간 선택 추가

--- src/routes/OSMDEditorRoute.tsx
+++ src/routes/OSMDEditorRoute.tsx
@@ -1,23 +1,51 @@
 import { UploadOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, Divider, Space } from 'antd';
+import CutMeasureInfo from 'components/CutMeasureInfo';
 import LoadSheetModal from 'components/LoadSheetModal';
 import ResponsiveCont from 'components/ResponsiveCont';
+import SheetEditor from 'components/SheetEditor';
+import { Size } from 'constants/layout';
+import { Sheet } from 'models/Sheet';
+import { loadSheetThunk } from 'modules/audio';
+import { State } from 'modules/State';
+import { useEffect } from 'react';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { MeasureRange } from 'utils/Editor';
 
 export default function OSMDEditorRoute() {
+    const dispatch = useDispatch();
   const [loadModal, setLoadModal] = useState(false);
-  const [sheetFile, setSheetFile] = useState<File | null>(null);
-
+  const [range, setRange] = useState<MeasureRange|null>(null);
+  const sheetKey = "asdf";// TODO createSheetKey function 
+  const sheet = useSelector(
+    (state: State) => (state.audio.sheets[sheetKey] ?? null) as Sheet | null,
+  );
+  
+  const showModal = (start:number, end:Number) => {
+    setRange({start,end} as MeasureRange);
+  };
+  useEffect(()=>{
+    setRange(null);
+  },[sheet]);
   return (
     <>
       <LoadSheetModal
         onLoadFile={(file) => {
-          setSheetFile(file.originFileObj as File);
+            dispatch(loadSheetThunk(sheetKey, file.originFileObj as File));
         }}
         visible={loadModal}
         onVisibleChange={setLoadModal}
       ></LoadSheetModal>
+      
       <ResponsiveCont>
+      <Space
+          direction="vertical"
+          size={8}
+          style={{
+            marginTop: 20,
+          }}
+        >
         <Button 
           onClick={() => {
             setLoadModal(true);
@@ -26,8 +54,22 @@ export default function OSMDEditorRoute() {
           <UploadOutlined />
           악보 업로드
         </Button>
-        
+          <CutMeasureInfo
+          sheetKey={sheetKey} 
+          range={range}
+        ></CutMeasureInfo>
+        </Space>
       </ResponsiveCont>
+      <Space
+          direction="vertical"
+          size={8}
+          style={{
+            marginTop: 20
+          }}
+        >
+        <Divider />
+        <SheetEditor showModal={showModal} sheetKey={sheetKey} range={range}></SheetEditor>
+      </Space>
     </>
   );
 }
