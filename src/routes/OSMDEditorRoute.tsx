a0f2709587e6d94bd2d2e8d7288ed8a45066b06e
chlorUpload <chlor.upload@gmail.com>
2021-09-05 20:24:42 +0900

    코드 관리

--- src/routes/OSMDEditorRoute.tsx
+++ src/routes/OSMDEditorRoute.tsx
@@ -4,7 +4,6 @@ import CutMeasureInfo from 'components/CutMeasureInfo';
 import LoadSheetModal from 'components/LoadSheetModal';
 import ResponsiveCont from 'components/ResponsiveCont';
 import SheetEditor from 'components/SheetEditor';
-import { Size } from 'constants/layout';
 import { Sheet } from 'models/Sheet';
 import { loadSheetThunk } from 'modules/audio';
 import { State } from 'modules/State';
@@ -14,61 +13,62 @@ import { useDispatch, useSelector } from 'react-redux';
 import { MeasureRange } from 'utils/Editor';
 
 export default function OSMDEditorRoute() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [loadModal, setLoadModal] = useState(false);
-  const [range, setRange] = useState<MeasureRange|null>(null);
-  const sheetKey = "asdf";// TODO createSheetKey function 
+  const [range, setRange] = useState<MeasureRange | null>(null);
+  const sheetKey = 'asdf'; // TODO createSheetKey function
   const sheet = useSelector(
     (state: State) => (state.audio.sheets[sheetKey] ?? null) as Sheet | null,
   );
-  
-  const showModal = (start:number, end:Number) => {
-    setRange({start,end} as MeasureRange);
+
+  const showModal = (start: number, end: Number) => {
+    setRange({ start, end } as MeasureRange);
   };
-  useEffect(()=>{
+  useEffect(() => {
     setRange(null);
-  },[sheet]);
+  }, [sheet]);
   return (
     <>
       <LoadSheetModal
         onLoadFile={(file) => {
-            dispatch(loadSheetThunk(sheetKey, file.originFileObj as File));
+          dispatch(loadSheetThunk(sheetKey, file.originFileObj as File));
         }}
         visible={loadModal}
         onVisibleChange={setLoadModal}
       ></LoadSheetModal>
-      
+
       <ResponsiveCont>
-      <Space
+        <Space
           direction="vertical"
           size={8}
           style={{
             marginTop: 20,
           }}
         >
-        <Button 
-          onClick={() => {
-            setLoadModal(true);
-          }}
-        >
-          <UploadOutlined />
-          악보 업로드
-        </Button>
-          <CutMeasureInfo
-          sheetKey={sheetKey} 
-          range={range}
-        ></CutMeasureInfo>
+          <Button
+            onClick={() => {
+              setLoadModal(true);
+            }}
+          >
+            <UploadOutlined />
+            악보 업로드
+          </Button>
+          <CutMeasureInfo sheetKey={sheetKey} range={range}></CutMeasureInfo>
         </Space>
       </ResponsiveCont>
       <Space
-          direction="vertical"
-          size={8}
-          style={{
-            marginTop: 20
-          }}
-        >
+        direction="vertical"
+        size={8}
+        style={{
+          marginTop: 20,
+        }}
+      >
         <Divider />
-        <SheetEditor showModal={showModal} sheetKey={sheetKey} range={range}></SheetEditor>
+        <SheetEditor
+          showModal={showModal}
+          sheetKey={sheetKey}
+          range={range}
+        ></SheetEditor>
       </Space>
     </>
   );
