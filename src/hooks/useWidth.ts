e6d57174bbad1b12da351efc6a3904fb54ecfbd0
chlorUpload <chlor.upload@gmail.com>
2021-09-05 20:23:28 +0900

    ReactiveCont 버벅거리는 오류 해결

--- src/hooks/useWidth.ts
+++ src/hooks/useWidth.ts
@@ -9,8 +9,28 @@ type WidthRes = {
   widthMode: WidthMode;
 };
 
+const getWidthInfo = (): WidthRes => {
+  const width = window.innerWidth;
+  let mode: WidthMode = WidthMode.Desktop;
+  if (width > Size.desktopMin) {
+    mode = WidthMode.Desktop;
+  } else if (width > Size.tabletMin) {
+    mode = WidthMode.Tablet;
+  } else {
+    mode = WidthMode.Mobile;
+  }
+  return { width, widthMode: mode };
+};
+
 export function useWidth(): WidthRes {
   const { width, widthMode } = useSelector((state: State) => state.layout);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const { width: w, widthMode: m } = getWidthInfo();
+    dispatch(setWidth(w));
+    dispatch(setWidthMode(m));
+  }, []);
   return { width, widthMode };
 }
 
@@ -18,17 +38,9 @@ export function useWidthStartup() {
   const dispatch = useDispatch();
 
   const handleResize = useCallback(() => {
-    const width = window.innerWidth;
-    let mode: WidthMode = WidthMode.Desktop;
-    if (width > Size.desktopMin) {
-      mode = WidthMode.Desktop;
-    } else if (width > Size.tabletMin) {
-      mode = WidthMode.Tablet;
-    } else {
-      mode = WidthMode.Mobile;
-    }
-    dispatch(setWidth(width));
-    dispatch(setWidthMode(mode));
+    const { width: w, widthMode: m } = getWidthInfo();
+    dispatch(setWidth(w));
+    dispatch(setWidthMode(m));
     // eslint-disable-next-line
   }, []);
 
