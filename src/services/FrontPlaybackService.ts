1729b46511c787cace719c0ff150647f1cdcdde6
chlorUpload <chlor.upload@gmail.com>
2021-09-13 00:05:42 +0900

    메트로눔 기능 추가

--- src/services/FrontPlaybackService.ts
+++ src/services/FrontPlaybackService.ts
@@ -20,64 +20,86 @@ export class FrontPlaybackService implements IPlaybackService {
     await this.engine.loadScore(osmd as any);
   }
 
+  public startMetronome() {
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.startMetronome();
+  }
+
+  public stopMetronome() {
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.stopMetronome();
+  }
+
   public play() {
-    if (this.engine !== null) {
-      this.engine.play();
-    } else throw new Error('osmd or playback engine is not initalized');
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.play();
   }
   public pause() {
-    if (this.engine !== null) {
-      this.engine.pause();
-    } else throw new Error('osmd or playback engine is not initalized');
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.pause();
   }
   public stop() {
-    if (this.engine !== null) {
-      this.engine.stop();
-    } else throw new Error('osmd or playback engine is not initalized');
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.stop();
   }
 
   public jumpToMeasure(measureInd: number) {
-    if (this.osmd !== null && this.engine !== null) {
-      this.engine.pause();
-      this.engine.jumpToMeasure(measureInd);
-      this.engine.play();
-      this.osmd.cursor.show();
-    } else {
+    if (this.osmd === null || this.engine === null)
       throw new Error('osmd or playback engine is not initalized');
-    }
+
+    this.engine.pause();
+    this.engine.jumpToMeasure(measureInd);
+    this.engine.play();
+    this.osmd.cursor.show();
+  }
+
+  public addMetronomeListener(listener: (metronome: boolean) => void) {
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.on(PlaybackEvent.METRONOME, listener);
   }
 
   public addIteratorListener(
     listener: (iterator: MusicPartManagerIterator) => void,
   ) {
-    if (this.engine !== null) {
-      this.engine.on(PlaybackEvent.ITERATION, listener);
-    }
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.on(PlaybackEvent.ITERATION, listener);
   }
 
   public addPlaybackStateListener(listener: (state: PlaybackState) => void) {
-    if (this.engine !== null) {
-      this.engine.on(
-        PlaybackEvent.STATE_CHANGE,
-        (state: EnginePlaybackState) => {
-          let res: PlaybackState | null = null;
-          switch (state) {
-            case EnginePlaybackState.INIT:
-              res = PlaybackState.INIT;
-              break;
-            case EnginePlaybackState.PLAYING:
-              res = PlaybackState.PLAYING;
-              break;
-            case EnginePlaybackState.PAUSED:
-              res = PlaybackState.PAUSED;
-              break;
-            case EnginePlaybackState.STOPPED:
-              res = PlaybackState.STOPPED;
-              break;
-          }
-          listener(res);
-        },
-      );
-    }
+    if (this.engine === null)
+      throw new Error('osmd or playback engine is not initalized');
+
+    this.engine.on(PlaybackEvent.STATE_CHANGE, (state: EnginePlaybackState) => {
+      let res: PlaybackState | null = null;
+      switch (state) {
+        case EnginePlaybackState.INIT:
+          res = PlaybackState.INIT;
+          break;
+        case EnginePlaybackState.PLAYING:
+          res = PlaybackState.PLAYING;
+          break;
+        case EnginePlaybackState.PAUSED:
+          res = PlaybackState.PAUSED;
+          break;
+        case EnginePlaybackState.STOPPED:
+          res = PlaybackState.STOPPED;
+          break;
+      }
+      listener(res);
+    });
   }
 }
