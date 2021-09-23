cb2c01a93382866b77cb16a27762f3c030e61705
chlorUpload <chlor.upload@gmail.com>
2021-09-23 22:25:16 +0900

    코드 스타일 유지

--- src/utils/KeyMap.ts
+++ src/utils/KeyMap.ts
@@ -1,7 +1,12 @@
-import { midiKeyNumberToNote, Note, noteToMidiKeyNumber, parseNoteNameToNote } from "./Note";
+import {
+  midiKeyNumberToNote,
+  Note,
+  noteToMidiKeyNumber,
+  parseNoteNameToNote,
+} from './Note';
 
-const top = "q2w3er5t6y7ui9o0p[=]" //C4
-const down = "zsxdcvgbhnjm,l.;/"; //C3
+const top = 'q2w3er5t6y7ui9o0p[=]'; //C4
+const down = 'zsxdcvgbhnjm,l.;/'; //C3
 
 const noteC3 = parseNoteNameToNote('C3');
 const noteC3MidiKeyNumber = noteToMidiKeyNumber(noteC3);
@@ -9,12 +14,12 @@ const noteC3MidiKeyNumber = noteToMidiKeyNumber(noteC3);
 const noteC4 = parseNoteNameToNote('C4');
 const noteC4MidiKeyNumber = noteToMidiKeyNumber(noteC4);
 
-export const keyBoardToNote = (key:string): (Note|null) =>{
-    if(top.indexOf(key) > -1){
-        return midiKeyNumberToNote(top.indexOf(key) + noteC4MidiKeyNumber);
-    } else if(down.indexOf(key) > -1){
-        return midiKeyNumberToNote(down.indexOf(key) + noteC3MidiKeyNumber);
-    } else {
-        return null;
-    }
-}
+export const keyboardToNote = (key: string): Note | null => {
+  if (top.indexOf(key) > -1) {
+    return midiKeyNumberToNote(top.indexOf(key) + noteC4MidiKeyNumber);
+  } else if (down.indexOf(key) > -1) {
+    return midiKeyNumberToNote(down.indexOf(key) + noteC3MidiKeyNumber);
+  } else {
+    return null;
+  }
+};
